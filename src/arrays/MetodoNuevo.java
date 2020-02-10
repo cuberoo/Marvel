5750b447bb4b7a216078764be9e53f372bdb138e
cubero <juancarloscuberoalamos@gmail.com>
2020-02-10 11:08:41 +0100

    NUEVO2

--- src/arrays/MetodoNuevo.java
+++ src/arrays/MetodoNuevo.java
@@ -4,8 +4,9 @@ public class MetodoNuevo {
 
 	public static void main(String[] args) {
 		int[]array = UtilsArrays.creaArrayNumerosAzar(10, 1, 10);
+		int[]array1 = UtilsArrays.creaArrayNumerosAzar(10, 1, 10);
 		UtilsArrays.mostrarArray(array);
-
+		UtilsArrays.mostrarArray(array1);
 	}
 
 }
