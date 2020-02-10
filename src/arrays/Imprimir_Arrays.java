adfd398f130c715dccca31215704a8c14687bc24
cubero <juancarloscuberoalamos@gmail.com>
2020-02-10 10:38:37 +0100

    segundo guardado

---
+++ src/arrays/Imprimir_Arrays.java
@@ -0,0 +1,11 @@
+package arrays;
+
+public class Imprimir_Arrays {
+
+	public static void main(String[] args) {
+
+		int[] array = UtilsArrays.creaArrayNumerosAzar(10, 0, 19);
+		UtilsArrays.mostrarArray(array);
+	}
+
+}
