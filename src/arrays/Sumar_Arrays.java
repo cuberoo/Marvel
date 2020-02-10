709b2d655ffad9a8412479b319c8983bd9f74783
cubero <juancarloscuberoalamos@gmail.com>
2020-02-10 10:45:04 +0100

    tercer guardado

---
+++ src/arrays/Sumar_Arrays.java
@@ -0,0 +1,10 @@
+package arrays;
+
+public class Sumar_Arrays {
+
+	public static void main(String[] args) {
+		int[] array = UtilsArrays.creaArrayNumerosAzar(20, 1, 20);
+		int[] array1 = UtilsArrays.creaArrayNumerosAzar(20, 1, 20);
+	}
+
+}
