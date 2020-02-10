8711fb05057e20d61fa1cb7390d2be148f9ea3d1
cubero <juancarloscuberoalamos@gmail.com>
2020-02-10 11:02:41 +0100

    nuevo

---
+++ src/arrays/MetodoNuevo.java
@@ -0,0 +1,11 @@
+package arrays;
+
+public class MetodoNuevo {
+
+	public static void main(String[] args) {
+		int[]array = UtilsArrays.creaArrayNumerosAzar(10, 1, 10);
+		UtilsArrays.mostrarArray(array);
+
+	}
+
+}
---
+++ src/arrays/NuevoMetodo.java
@@ -0,0 +1,12 @@
+package arrays;
+
+public class NuevoMetodo {
+
+	public static void main(String[] args) {
+		int[]array = UtilsArrays.creaArrayNumerosAzar(10, 1, 10);
+		UtilsArrays.mostrarArray(array);
+
+
+	}
+
+}
